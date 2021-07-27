f97858b4b8e43dadcc68293918e45efeb45b7f62
kanha91 <kanhasingh91@gmail.com>
2021-07-27 18:55:54 +0530

    root_url

--- src/app/web-request.service.ts
+++ src/app/web-request.service.ts
@@ -9,7 +9,8 @@ export class WebRequestService {
   readonly ROOT_URL;
 
   constructor(private http: HttpClient) {
-    this.ROOT_URL = 'http://localhost:8080';
+    // this.ROOT_URL = 'http://localhost:8080';
+    this.ROOT_URL = 'https://myapi-kanha.herokuapp.com/';
    }
 
 
