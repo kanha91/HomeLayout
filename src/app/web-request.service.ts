e644018a755074d7e0a31b3ec36f3d19837d4307
kanha91 <54071847+kanha91@users.noreply.github.com>
2021-07-28 12:13:01 +0530

    Update web-request.service.ts

--- src/app/web-request.service.ts
+++ src/app/web-request.service.ts
@@ -9,7 +9,7 @@ export class WebRequestService {
   readonly ROOT_URL;
 
   constructor(private http: HttpClient) {
-    this.ROOT_URL = 'http://localhost:4050';
+    this.ROOT_URL = 'https://myapi-kanha.herokuapp.com';
    }
 
 
